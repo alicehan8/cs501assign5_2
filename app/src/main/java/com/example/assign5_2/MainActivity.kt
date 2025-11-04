ecf9864b7d2c17fe00670e290132fc2cc4207ad7
alicehan8 <alicehan@bu.edu>
2025-11-04 13:40:07 -0500

    pages completed

--- app/src/main/java/com/example/assign5_2/MainActivity.kt
+++ app/src/main/java/com/example/assign5_2/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.DateRange
@@ -13,16 +18,24 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -31,13 +44,18 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.assign5_2.ui.theme.Assign5_2Theme
 
+
+data class CheckableItem(var label: String, var isChecked: Boolean = false)
+
+
 class MainActivity : ComponentActivity() {
+    private val viewModel: MyViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Assign5_2Theme {
-                MainScreen()
+                MainScreen(viewModel)
             }
         }
     }
@@ -56,8 +74,26 @@ val screens = listOf(
     Screen.Calendar
 )
 
+class MyViewModel : ViewModel() {
+    var notes by mutableStateOf(listOf<String>())
+    var tasks by mutableStateOf(listOf<CheckableItem>())
+
+    fun addNote(note: String) {
+        notes = notes + note
+    }
+    fun addTask(task: CheckableItem) {
+        tasks = tasks + task
+    }
+    fun toggleTaskChecked(index: Int, isChecked: Boolean) {
+        val updatedTasks = tasks.toMutableList()
+        val updatedItem = updatedTasks[index].copy(isChecked = isChecked)
+        updatedTasks[index] = updatedItem
+        tasks = updatedTasks
+    }
+}
+
 @Composable
-fun MainScreen(){
+fun MainScreen(viewModel: MyViewModel){
     // 1. Create a NavController. `rememberNavController()` creates and remembers it
     // across recompositions. This is the heart of our navigation system.
     val navController = rememberNavController()
@@ -66,6 +102,8 @@ fun MainScreen(){
     // floating action buttons, and the main content.
     Scaffold(
         bottomBar = {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route?.substringBefore('?') // normalize
             // Our custom bottom navigation bar.
             NavigationBar {
                 // 3. Get the current back stack entry. This tells us which screen is currently displayed.
@@ -111,9 +149,65 @@ fun MainScreen(){
             modifier = Modifier.padding(innerPadding) // Apply padding from the Scaffold.
         ) {
             // Define a composable for each screen in our navigation graph.
-            composable(Screen.Notes.route) { Text("Notes") }
-            composable(Screen.Tasks.route) { Text("Tasks") }
+            composable(Screen.Notes.route) { NotesPage(viewModel)}
+            composable(Screen.Tasks.route) { TaskPage(viewModel) }
             composable(Screen.Calendar.route) { Text("Calendar") }
+        }
+    }
+}
+
+@Composable
+fun NotesPage(viewModel: MyViewModel){
+    var note by remember { mutableStateOf("") }
+    Column(Modifier.fillMaxHeight()){
+        Text("Notes")
+        LazyColumn {
+            items(viewModel.notes.size) { index ->
+                Text(viewModel.notes[index])
+            }
+        }
+        TextField(
+            value = note,
+            onValueChange = { note = it },
+            label = { Text("Add a note")}
+        )
+        Button(onClick = {
+            viewModel.addNote(note)
+            note = ""
+        }) {
+            Text("Add Note")
+        }
+    }
+}
+
+@Composable
+fun TaskPage(viewModel: MyViewModel){
+    var taskLabel by remember { mutableStateOf("") }
+    Column(Modifier.fillMaxHeight()){
+        Text("Tasks")
+        LazyColumn {
+            items(viewModel.tasks.size) { index ->
+                Row() {
+                    Checkbox(
+                        checked = viewModel.tasks[index].isChecked,
+                        onCheckedChange = {
+                            viewModel.toggleTaskChecked(index, it)
+                        }
+                    )
+                    Text(viewModel.tasks[index].label)
+                }
+            }
+        }
+        TextField(
+            value = taskLabel,
+            onValueChange = { taskLabel = it },
+            label = { Text("Add a task")}
+        )
+        Button(onClick = {
+            viewModel.addTask(task = CheckableItem(taskLabel))
+            taskLabel = ""
+        }) {
+            Text("Add Task")
         }
     }
 }
